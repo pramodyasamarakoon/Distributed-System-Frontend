e9870f8067342befda06dfef21162b1900a96f3b
Pramodya Samarakoon <pramodyasamarakoon2516@gmail.com>
2024-03-10 09:02:09 +0530

    name changed

--- src/Components/Logo/index.jsx
+++ src/Components/Logo/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const index = ({ text = "text-[#1E1F6F]" }) => {
   return (
     <div>
-      <p className={`text-2xl font-Poppins-SemiBold ${text} `}>Kumi Drive</p>
+      <p className={`text-2xl font-Poppins-SemiBold ${text} `}>S3 Safe</p>
     </div>
   );
 };
